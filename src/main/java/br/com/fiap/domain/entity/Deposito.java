2f85c2f9f0429f3ae554b00b0fbabd7dccb02e37
Alberto <albertoperuchi11@gmail.com>
2023-08-10 11:42:53 -0300

    ok

---
+++ src/main/java/br/com/fiap/domain/entity/Deposito.java
@@ -0,0 +1,41 @@
+package br.com.fiap.domain.entity;
+
+public class Deposito {
+
+    private Long id;
+    private String nome;
+
+    public Deposito() {
+    }
+
+    public Deposito(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Deposito{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
